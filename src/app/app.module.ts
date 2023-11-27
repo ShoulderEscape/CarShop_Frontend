7313dfdea440ff7d565fdd8ff05ffd0a12264e70
ShoulderEscape <105722638+ShoulderEscape@users.noreply.github.com>
2023-11-27 13:55:53 +0100

    import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from 'src/components/footer/footer.component';
-import { HeaderComponent } from 'src/components/header/header.component';
-import { AboutComponent } from 'src/components/about/about.component';
-import { CarComponent } from 'src/components/car/car.component';
-import { CarFormComponent } from 'src/components/car-form/car-form.component';
-import { HomeComponent } from 'src/components/home/home.component';
-import { FilterComponent } from 'src/components/home/filter/filter.component';
-import { LoginComponent } from 'src/components/login/login.component';
-import { ProfileComponent } from 'src/components/profile/profile.component';
+import { FooterComponent } from 'src/components/partial-views/footer/footer.component';
+import { HeaderComponent } from 'src/components/partial-views/header/header.component';
+import { AboutComponent } from 'src/components/pages/about/about.component';
+import { CarComponent } from 'src/components/partial-views/car/car.component';
+import { CarFormComponent } from 'src/components/pages/car-form/car-form.component';
+import { HomeComponent } from 'src/components/pages/home/home.component';
+import { FilterComponent } from 'src/components/pages/home/filter/filter.component';
+import { LoginComponent } from 'src/components/pages/login/login.component';
+import { ProfileComponent } from 'src/components/pages/profile/profile.component';
 
 @NgModule({
   declarations: [
