27c01cf6594250b5e14ed22c09e26347ddbef4be
ShoulderEscape <105722638+ShoulderEscape@users.noreply.github.com>
2023-11-27 14:08:51 +0100

    styling fix

--- src/components/pages/car-form/car-form.component.ts
+++ src/components/pages/car-form/car-form.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-car-form',
   templateUrl: './car-form.component.html',
-  styleUrls: ['./car-form.component.sass']
+  styleUrls: ['./car-form.component.css']
 })
 export class CarFormComponent {
 
