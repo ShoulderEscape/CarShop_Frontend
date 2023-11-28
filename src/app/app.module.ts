c67b16680897f62842596b17ad452c1fa6b131d0
NiclasEdstrom <113895402+NiclasEdstrom@users.noreply.github.com>
2023-11-28 20:41:10 +0100

    footerinfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from 'src/components/partial-views/footer/footer.component';
-import { HeaderComponent } from 'src/components/partial-views/header/header.component';
-import { AboutComponent } from 'src/components/pages/about/about.component';
-import { CarComponent } from 'src/components/partial-views/car/car.component';
-import { CarFormComponent } from 'src/components/pages/car-form/car-form.component';
-import { HomeComponent } from 'src/components/pages/home/home.component';
-import { FilterComponent } from 'src/components/pages/home/filter/filter.component';
-import { LoginComponent } from 'src/components/pages/login/login.component';
-import { ProfileComponent } from 'src/components/pages/profile/profile.component';
+import { FooterComponent } from 'src/components/partial-views/footer/footer.component'; 
+import { HeaderComponent } from 'src/components/partial-views/header/header.component'; 
+import { AboutComponent } from 'src/components/pages/about/about.component'; 
+import { CarComponent } from 'src/components/partial-views/car/car.component'; 
+import { CarFormComponent } from 'src/components/pages/car-form/car-form.component'; 
+import { HomeComponent } from 'src/components/pages/home/home.component'; 
+import { FilterComponent } from 'src/components/pages/home/filter/filter.component'; 
+import { LoginComponent } from 'src/components/pages/login/login.component'; 
+import { ProfileComponent } from 'src/components/pages/profile/profile.component'; 
 
 @NgModule({
   declarations: [
@@ -29,6 +29,7 @@ import { ProfileComponent } from 'src/components/pages/profile/profile.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent],
